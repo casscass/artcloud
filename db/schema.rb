d3016662f2965818b83aa5947480363c6e00f95c
cassandra <cassandradaw@gmail.com>
2018-05-04 12:39:51 +1000

    scaffold Profile user:string f_name:string l_name:string username:string phone:string
    end
    quit
    endquote
    quote
    exit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_04_005255) do
+ActiveRecord::Schema.define(version: 2018_05_04_013850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2018_05_04_005255) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_artworks_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "username"
+    t.string "f_name"
+    t.string "l_name"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -46,4 +57,5 @@ ActiveRecord::Schema.define(version: 2018_05_04_005255) do
   end
 
   add_foreign_key "artworks", "users"
+  add_foreign_key "profiles", "users"
 end
