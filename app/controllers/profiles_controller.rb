a3fa611f8820edc5ee7ee738027c7c8f93566cef
cassandra <cassandradaw@gmail.com>
2018-05-04 12:43:59 +1000

    In profiles_controller def change. Changed  @profile = Profile.new(profile_params) to  @profile = current_user.build_profile(profile_params)

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -25,6 +25,7 @@ class ProfilesController < ApplicationController
   # POST /profiles.json
   def create
     @profile = current_user.build_profile(profile_params)
+   
 
     respond_to do |format|
       if @profile.save
