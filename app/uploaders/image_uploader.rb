19a26ca3b61dfc3d5394afb1bc64c3b109281741
cassandra <cassandradaw@gmail.com>
2018-05-10 09:39:43 +1000

    sendgrid setup- stil trying to send an email

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,10 +34,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fill: [50, 50]
+    process resize_to_fill: [10, 10]
   end
   version :small_thumb, from_version: :thumb do
-    process resize_to_fill: [30, 30]
+    process resize_to_fill: [10, 10]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
